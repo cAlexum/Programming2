9f1f97a5806a2858f2cfacfff684d91669bdbf17
cAlexum <153202415+cAlexum@users.noreply.github.com>
2023-12-16 10:11:00 +0000

    Update app.js
    Updated javascript

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const RandNum = Math.floor(Math.random() * 100) + 1;
 
 let Lives = parseInt(document.getElementsByClassName("lastResult")[0].innerText);
-let totalGuesses = []
+let totalGuesses = [];
 
 const GuessFunction = (e) => {
     e.preventDefault();
     let Guess = document.getElementById("guessField").value;
-    if (Guess !== RandNum) {
-        Lives = Lives - 1;
-        document.getElementsByClassName("lastResult")[0].innerText = Lives
-        totalGuesses.push(Guess)
-        document.getElementsByClassName("guesses")[0].innerText = totalGuesses.toString();
+    if (Lives == 0) {
+        document.getElementById("subt").disabled = true;
+        alert("You lost!");
     }
-    if (Guess == RandNum) {
+    else if ( Guess in totalGuesses || Guess < 1) {
+      
+    }
+    else if (Guess !== RandNum) {
+        Lives = Lives - 1;
+        document.getElementsByClassName("lastResult")[0].innerText = Lives;
+        totalGuesses.push(Guess);
+        for (let i = 0; i < totalGuesses.length; i++) {
+            for (let j = 0; j< totalGuesses.length; j++) {
+              if (i == j) {}
+              else if (totalGuesses[i] == totalGuesses[j] && i !== 0) {
+                totalGuesses.splice(i,1);
+                break
+              }
+              else if (totalGuesses[i] == totalGuesses[j] && i == 0) {
+                totalGuesses.splice(0,1)
+          }
+            }
+          } 
+          totalGuesses.sort();
+          document.getElementsByClassName("guesses")[0].innerText = totalGuesses.toString();
+    }
+    else if (Guess == RandNum) {
         alert("You Won!");
     }
-    if (Lives == 0) {document.getElementById("subt").disabled = true}
     };
+    ;
 document.getElementById("subt").addEventListener("click", GuessFunction);
